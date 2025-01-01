bc308bee7a57d35e215379a5245f5461d0b33e72
Đặng Minh Kha <skymost21@gmail.com>
2025-01-01 21:39:33 +0700

    f

--- src/services/base-service.ts
+++ src/services/base-service.ts
@@ -1,129 +1,131 @@
-import axios from "axios";
-import router from "../router";
-import { useBaseStore } from "../stores/baseStore";
+import axios from 'axios'
+import router from '../router'
+import { useBaseStore } from '../stores/baseStore'
 const addTokenHeader = () => {
   return {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Methods": "POST,PATCH,OPTIONS",
-    "Secret-Key": "okyo-secret-key",
-  };
-};
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Methods': 'POST,PATCH,OPTIONS',
+    'Secret-Key': 'T0tZMCRFY1JFdGtleTIwMjU=',
+  }
+}
 const getHeader = () => {
-  const baseStore = useBaseStore();
+  const baseStore = useBaseStore()
   return {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Methods": "POST,PATCH,OPTIONS",
-    "Secret-Key": "okyo-secret-key",
-    Authorization: baseStore.$state.loginData ? "Bearer " + baseStore.$state.loginData.data.accessToken : '',
-  };
-};
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Methods': 'POST,PATCH,OPTIONS',
+    'Secret-Key': 'T0tZMCRFY1JFdGtleTIwMjU=',
+    Authorization: baseStore.$state.loginData
+      ? 'Bearer ' + baseStore.$state.loginData.data.accessToken
+      : '',
+  }
+}
 const getHeaderFile = () => {
   return {
-    "Content-Type": "multipart/form-data",
-    "Access-Control-Allow-Methods": "POST,PATCH,OPTIONS",
-    "Secret-Key": "okyo-secret-key",
-  };
-};
+    'Content-Type': 'multipart/form-data',
+    'Access-Control-Allow-Methods': 'POST,PATCH,OPTIONS',
+    'Secret-Key': 'T0tZMCRFY1JFdGtleTIwMjU=',
+  }
+}
 async function fetch(url: string) {
-  const header = getHeader();
-  const baseStore = useBaseStore();
-  baseStore.overlayChange(true);
+  const header = getHeader()
+  const baseStore = useBaseStore()
+  baseStore.overlayChange(true)
   try {
-    const response = await axios.get(url, { headers: header });
-    return handleSuccess(response);
+    const response = await axios.get(url, { headers: header })
+    return handleSuccess(response)
   } catch (error) {
-    return handleError(error);
+    return handleError(error)
   }
 }
 async function fetchParams(url: string, params: any) {
-  const header = getHeader();
-  const baseStore = useBaseStore();
-  baseStore.overlayChange(true);
+  const header = getHeader()
+  const baseStore = useBaseStore()
+  baseStore.overlayChange(true)
   try {
-    const response = await axios.get(url, { headers: header, params: params });
-    return handleSuccess(response);
+    const response = await axios.get(url, { headers: header, params: params })
+    return handleSuccess(response)
   } catch (error) {
-    return handleError(error);
+    return handleError(error)
   }
 }
 async function addAuthen(url: string, params: any) {
-  const header = addTokenHeader();
-  const baseStore = useBaseStore();
-  baseStore.overlayChange(true);
+  const header = addTokenHeader()
+  const baseStore = useBaseStore()
+  baseStore.overlayChange(true)
   try {
-    const response = await axios.post(url, params, { headers: header });
-    return handleSuccess(response);
+    const response = await axios.post(url, params, { headers: header })
+    return handleSuccess(response)
   } catch (error) {
-    return handleError(error);
+    return handleError(error)
   }
 }
 async function add(url: string, params: any) {
-  const header = getHeader();
-  const baseStore = useBaseStore();
-  baseStore.overlayChange(true);
+  const header = getHeader()
+  const baseStore = useBaseStore()
+  baseStore.overlayChange(true)
   try {
-    const response = await axios.post(url, params, { headers: header });
-    return handleSuccess(response);
+    const response = await axios.post(url, params, { headers: header })
+    return handleSuccess(response)
   } catch (error) {
-    return handleError(error);
+    return handleError(error)
   }
 }
 //post file
 async function addFile(url: string, params: any) {
-  const header = getHeaderFile();
-  const baseStore = useBaseStore();
-  baseStore.overlayChange(true);
-  var formData = new FormData();
-  formData.append("file", params);
+  const header = getHeaderFile()
+  const baseStore = useBaseStore()
+  baseStore.overlayChange(true)
+  var formData = new FormData()
+  formData.append('file', params)
   try {
-    const response = await axios.post(url, formData, { headers: header });
-    return handleSuccess(response);
+    const response = await axios.post(url, formData, { headers: header })
+    return handleSuccess(response)
   } catch (error) {
-    return handleError(error);
+    return handleError(error)
   }
 }
 async function update(url: string, params: any) {
-  const header = getHeader();
-  const baseStore = useBaseStore();
-  baseStore.overlayChange(true);
+  const header = getHeader()
+  const baseStore = useBaseStore()
+  baseStore.overlayChange(true)
   try {
-    const response = await axios.put(url, params, { headers: header });
-    return handleSuccess(response);
+    const response = await axios.put(url, params, { headers: header })
+    return handleSuccess(response)
   } catch (error) {
-    return handleError(error);
+    return handleError(error)
   }
 }
 async function remove(url: string) {
-  const header = getHeader();
-  const baseStore = useBaseStore();
-  baseStore.overlayChange(true);
+  const header = getHeader()
+  const baseStore = useBaseStore()
+  baseStore.overlayChange(true)
   try {
     const response = await axios.delete(url, {
       headers: header,
-    });
-    return handleSuccess(response);
+    })
+    return handleSuccess(response)
   } catch (error) {
-    return handleError(error);
+    return handleError(error)
   }
 }
 const handleSuccess = (data: any) => {
-  const baseStore = useBaseStore();
-  baseStore.overlayChange(false);
-  return data;
-};
+  const baseStore = useBaseStore()
+  baseStore.overlayChange(false)
+  return data
+}
 const handleError = (data: any) => {
-  const baseStore = useBaseStore();
-  baseStore.overlayChange(false);
+  const baseStore = useBaseStore()
+  baseStore.overlayChange(false)
   if (data?.status === 403) {
-    router.push({ path: "/login" });
+    router.push({ path: '/login' })
   }
   baseStore.snackChange({
     status: true,
     message: data.response.data.message,
-    color: "red",
-  });
-  return data;
-};
+    color: 'red',
+  })
+  return data
+}
 export const baseService = {
   fetch,
   fetchParams,
@@ -131,5 +133,5 @@ export const baseService = {
   addAuthen,
   addFile,
   update,
-  remove
-};
+  remove,
+}
